78654659955226fcfe2a53ab38bd39c052deff80
Bill7395 <gabrielexecutive7395@gmail.com>
2025-02-17 16:22:57 -0300

    new feature

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require('express')
-const port = process.eventNames.PORT || 3001
-const app = express()
+const express = require('express');
+const port = process.env.PORT || 3001;  // Corrigido para 'process.env.PORT'
+const app = express();
 
-app.listen(port, {} == {
-    console.log('start  listening ${port}')
+app.get('/',(req, res) => {
+    console.log('New feature')
 })
+
+app.listen(port, () => {  // Corrigido para uma função de callback corretamente
+    console.log(`Start listening on port ${port}`);  // Corrigido o uso das aspas e a interpolação de string
+});
